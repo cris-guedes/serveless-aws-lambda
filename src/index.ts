b70abc9d81b7f2ca655b6982ff25ef42b4454805
cris-guedes <cristianguedes136@gmail.com>
2022-04-05 14:55:06 -0300

    WIP

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const lambdaHandler = async () => {
+export const Handler = async () => {
   console.log(process.env.BASE_ENDPOINT_URL);
   console.log(process.env.AUTH_USER);
   console.log(process.env.AUTH_PASSWORD);
